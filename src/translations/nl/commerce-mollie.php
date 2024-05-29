bf5cad094600235374dfafc62bd09a209fb63272
Wesley van Gogh <wesley@gethooked.nl>
2024-05-29 10:51:01 +0200

    feat. Add Dutch translations for Mollie request response

---
+++ src/translations/nl/commerce-mollie.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'The payment was canceled.' => 'De betaling was geannuleerd.',
+    'The payment failed.' => 'De betaling is mislukt.',
+    'The payment expired.' => 'De betaling is verlopen.',
+    'Completing purchase is not supported by this gateway' =>
+    'Aankoop voltooien wordt niet ondersteund door deze betaalgateway',
+];
